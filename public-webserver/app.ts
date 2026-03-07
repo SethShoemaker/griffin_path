da2f293664ed25558c9b49f957125bcda0fef876
Seth Shoemaker <contact@sethshoemaker.com>
2026-03-06 22:48:43 -0500

    pagination

--- public-webserver/app.ts
+++ public-webserver/app.ts
@@ -21,7 +21,20 @@ app.engine('handlebars', engine({
             if (Array.isArray(array) && array.includes(value)) return true;
             return false;
         },
-        json: (ctx: any) => JSON.stringify(ctx)
+        json: (ctx: any) => JSON.stringify(ctx),
+        add: (a: any, b: any) => a + b,
+        subtract: (a: any, b: any) => a - b,
+        lte: (a: any, b: any) => a <= b,
+        gte: (a: any, b: any) => a >= b,
+        range: (start: any, end: any) => {
+            const arr = [];
+            for (let i = start; i <= end; i++) arr.push(i);
+            return arr;
+        },
+        and: (a: any, b: any) => a && b,
+        array: (...args: any[]) => args.slice(0, -1),
+        min: (a: any, b: any) => Math.min(a, b),
+        max: (a: any, b: any) => Math.max(a, b)
     }
 }));
 app.set('view engine', 'handlebars');
