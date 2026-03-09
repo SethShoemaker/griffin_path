4506f2abcf049d468a5ae071ffd5c2c7f45a13a0
Seth Shoemaker <contact@sethshoemaker.com>
2026-03-09 14:16:54 -0400

    improved multi_select_or styling

--- public-webserver/routers/section-search.ts
+++ public-webserver/routers/section-search.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from "express";
+import { queryParamValueArray } from "../helpers/query-params";
 
 export const sectionSearchRouter = express.Router();
 
@@ -15,6 +16,37 @@ sectionSearchRouter.get('/', async (request: Request, res: Response) => {
 
     res.render('section-search', {...internalApiResponseJson, ...{
         title: "Class Search",
+        filters: internalApiResponseJson.filters
+            .map((filter: any) => {
+                switch(filter.type) {
+                    case "text_search":
+                        return {
+                            type: "text_search",
+                            label: filter.name,
+                            inputName: filter.slug,
+                            placeholder: `Search ${filter.name}`,
+                            value: request.query[filter.slug] ?? null
+                        };
+                    case "multi_select_or":
+                        const values = queryParamValueArray(filter.slug, request)
+                        return {
+                            type: "multi_select_or",
+                            label: filter.name,
+                            inputName: filter.slug,
+                            placeholder: values.length > 0 ? values.map(v => v ? v : '(none)').join(',') : `Select ${filter.name}`,
+                            inputOptions: filter.inputOptions
+                                .map((inputOption: any) => ({
+                                    value: inputOption ? inputOption : 'null',
+                                    selected: values.includes(inputOption),
+                                    label: inputOption ? inputOption : '(none)'
+                                }))
+                        };
+                    default:
+                        console.log(`found unhandled filter type ${filter.type}, this needs to be resolved immediately`);
+                        return null;
+                }
+            })
+            .filter((filter: any) => filter != null),
         query: request.query
     },});
 });
